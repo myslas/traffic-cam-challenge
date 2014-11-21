cb0b110b62d88ba668f1d4ef98a28338516c0aa1
myslas <baseballkid-88@D-128-208-138-47.dhcp4.washington.edu>
2014-11-20 17:00:19 -0800

    seattle trafic cams via google maps

--- js/app.js
+++ js/app.js
@@ -11,3 +11,63 @@
 //you should also write the code to filter the set of markers when the user
 //types a search phrase into the search box
 
+$(document).ready(function() {
+    var markers = [];
+    var mapElem = document.getElementById('map');
+    var center = {
+        lat: 47.6,
+        lng: -122.3
+    };
+
+    var map = new google.maps.Map(mapElem, {
+        center: center,
+        zoom: 12
+    });
+
+    var infoWindow= new google.maps.InfoWindow();
+
+    $.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
+        .done(function(data) {
+            data.forEach(function(station) {
+                var marker = new google.maps.Marker({
+                    position: {
+                        lat: Number(station.location.latitude),
+                        lng: Number(station.location.longitude)
+                    },
+                    map: map,
+                    label: station.cameralabel,
+                    icon: 'camera.png'
+                });
+                markers.push(marker);
+                google.maps.event.addListener(marker, 'click', function() {
+                    map.panTo(this.getPosition());
+                    var html = '<h2>' + station.cameralabel + '</h2>';
+                    html += '<img src="' + station.imageurl.url + '"></img>'
+                    infoWindow.setContent(html);
+                    infoWindow.open(map, this);
+                });
+            });
+        })
+        .fail(function(error) {
+            console.log(error)
+        })
+        .always(function() {
+            $('#ajax-loader').fadeOut();
+        });
+
+    $('#search').bind('search keyup', function() {
+        var search = $('#search').val().toLowerCase();
+        for (var i = 0; i < markers.length; ++i) {
+            if(markers[i].label.toLowerCase().indexOf(search) == -1) {
+                markers[i].setMap(null);
+            } else {
+                markers[i].setMap(map)
+            }
+
+        }
+
+    });
+    google.maps.event.addListener(map, 'click', function() {
+        infoWindow.close();
+    })
+});
